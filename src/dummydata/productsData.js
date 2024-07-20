64d82b39b7db63e0e30b94b11ef7fe46402b5f09
unknown <imzahrakhoshdel@gmail.com>
2024-07-20 23:59:06 +0330

    adding products dummy data

---
+++ src/dummydata/productsData.js
@@ -0,0 +1,87 @@
+import image1 from "../assets/products/small_product-TLP-24095.png";
+import image2 from "../assets/products/small_product-TLP-24533.png";
+import image3 from "../assets/products/small_product-TLP-29457.png";
+import image4 from "../assets/products/small_product-TLP-27996.png";
+import image5 from "../assets/products/small_product-TLP-29297.png";
+import image6 from "../assets/products/small_product-TLP-29300.png";
+import image7 from "../assets/products/small_product-TLP-25176.png";
+import image8 from "../assets/products/small_product-TLP-3315.png";
+import image9 from "../assets/products/small_product-TLP-38828.png";
+import image10 from "../assets/products/small_product-TLP-20666.png";
+import image11 from "../assets/products/small_product-TLP-26429.png";
+import image12 from "../assets/products/small_product-TLP-44361.png";
+
+export const productsData = [
+  {
+    id: "TLP-24095",
+    name: "ساعت هوشمند تی سی اچ مدل Z10",
+    image: image1,
+    price: "760,000",
+  },
+  {
+    id: "TLP-24533",
+    name: "ساعت هوشمند شیائومی مدل Redmi Watch 3 Active",
+    image: image2,
+    price: "1,900,000",
+  },
+  {
+    id: "TLP-25176",
+    name: "ساعت هوشمند سامسونگ مدل Galaxy Watch6 40mm",
+    image: image3,
+    price: "8,089,000",
+  },
+  {
+    id: "TLP-27996",
+    name: "ساعت هوشمند میبرو مدل T2",
+    image: image4,
+    price: "2,400,000",
+  },
+  {
+    id: "TLP-29297",
+    name: "ساعت هوشمند مدل HK9 Pro Plus",
+    image: image5,
+    price: "1,400,000",
+  },
+  {
+    id: "TLP-29300",
+    name: "ساعت هوشمند اچ کی مدل HK9 Ultra 2 Max",
+    image: image6,
+    price: "1,800,000",
+  },
+  {
+    id: "TLP-29457",
+    name: "اپل واچ سری 9 مدل 45 میلی متری با بند سیلیکونی",
+    image: image7,
+    price: "20,499,000",
+  },
+  {
+    id: "TLP-3315",
+    name: "ساعت هوشمند سامسونگ مدل Galaxy Watch4 Classic 46mm",
+    image: image8,
+    price: "5,700,000",
+  },
+  {
+    id: "TLP-38828",
+    name: "ساعت هوشمند اچ کی مدل HK9 Pro Max Plus",
+    image: image9,
+    price: "1,700,000",
+  },
+  {
+    id: "TLP-20666",
+    name: "ساعت هوشمند مدل W26 Pro Max با هندزفری بلوتوث",
+    image: image10,
+    price: "375,000",
+  },
+  {
+    id: "TLP-26429",
+    name: "ساعت هوشمند مدل T900 Ultra",
+    image: image11,
+    price: "310,000",
+  },
+  {
+    id: "TLP-44361",
+    name: "ساعت هوشمند گلوریمی مدل M2 MAX",
+    image: image12,
+    price: "2,799,000",
+  },
+];
