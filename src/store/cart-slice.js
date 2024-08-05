7b809add3664180bb62c71c733f05db65955d0b2
unknown <imzahrakhoshdel@gmail.com>
2024-08-05 14:32:46 +0330

    fixing the bug in removing products from the cart

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -31,7 +31,7 @@ const cartSlice = createSlice({
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
       if (existingItem.quantity === 1) {
-        state.items.filter((item) => item.id !== id);
+        state.items = state.items.filter((item) => item.id !== id);
       } else {
         existingItem.quantity--;
         existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
