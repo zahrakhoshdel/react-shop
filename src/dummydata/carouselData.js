6c59125a4d1de79c6fbd531a2cd57358ae9ed2cc
unknown <imzahrakhoshdel@gmail.com>
2024-08-20 15:31:23 +0330

    adding some content

--- src/dummydata/carouselData.js
+++ src/dummydata/carouselData.js
@@ -1,14 +1,18 @@
+import carouselBanner1 from "../assets/carousel-banner1.png";
+import carouselBanner2 from "../assets/carousel-banner2.jpg";
+import carouselBanner3 from "../assets/carousel-banner3.png";
+
 export const caroselData = [
   {
-    url: "https://picsum.photos/seed/img1/1200/400",
+    url: carouselBanner1,
     alt: "image 1 for carousel",
   },
   {
-    url: "https://picsum.photos/seed/img2/1200/400",
+    url: carouselBanner2,
     alt: "image 2 for carousel",
   },
   {
-    url: "https://picsum.photos/seed/img3/1200/400",
+    url: carouselBanner3,
     alt: "image 3 for carousel",
   },
 ];
